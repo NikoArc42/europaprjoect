aba790e93c7fd53d1b9a63016c1054de7eddd468
NikoArc42 <nikoarc42@gmail.com>
2025-09-18 10:58:53 +0200

    sobbing

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function dropfunc() {
 
 window.onclick = function(event) {
     if (!event.target.matches('.ham')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
+      let dropdown = document.getElementsByClassName("dropdown-content");
+      let i;
+      for (i = 0; i < dropdown.length; i++) {
+        let openDropdown = dropdown[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
         }
