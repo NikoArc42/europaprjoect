c2d6a1ab75389469baec734da7161526f57a6761
NikoArc42 <nikoarc42@gmail.com>
2025-09-23 09:44:14 +0200

    plis gud

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-function dropfunc() {
-    document.getElementById("dropdown").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-    if (!event.target.matches('.ham')) {
-      let dropdown = document.getElementsByClassName("dropdown-content");
-      let i;
-      for (i = 0; i < dropdown.length; i++) {
-        let openDropdown = dropdown[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+const hamburger = document.querySelector('.menu');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+});
